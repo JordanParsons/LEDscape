dd9bdb0a051d8295c2735a22d0793ada370c028c
Jordan C. Parsons <jordancparsons@gmail.com>
2016-09-28 10:47:34 -0400

    Service update

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 cd $(dirname $0)
 
 if [[ -f ledscape.service ]]; then
-	echo "Stopping Service..."
-	systemctl stop ledscape.service
+	echo "Enabling Service..."
+	systemctl enable $(pwd)/ledscape.service || exit -1
 
-	echo "Disabling Service..."
-	systemctl disable $(pwd)/ledscape.service || exit -1
+	echo "Starting Service..."
+	systemctl start ledscape.service
+	systemctl status ledscape.service
 else
 	echo "Could not find ledscape.service. Please run make first"
 	exit -1
